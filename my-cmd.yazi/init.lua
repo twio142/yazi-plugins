f07836f46a5f1290ddd3da812ef121ba98c4bbab
twio142 <64556708+twio142@users.noreply.github.com>
2025-01-17 22:24:43 +0100

    style: format code

--- my-cmd.yazi/init.lua
+++ my-cmd.yazi/init.lua
@@ -1,206 +1,222 @@
 --- @sync entry
+--- @diagnostic disable: undefined-global
 _G.ya = _G.ya or {}
 _G.cx = _G.cx or {}
-_G.Command = _G.Command or {}
 
 local M = {}
 
 M.on_selection = function(mode)
-  local selected = #cx.active.selected
-  for i = 1, #cx.tabs do
-    for _, url in pairs(cx.tabs[i].selected) do
-      ya.manager_emit("toggle", { tostring(url), state = "on" })
-      selected = selected + 1
-    end
-  end
-  if selected == 0 then
-    return
-  end
-  local h = cx.active.current.hovered
-  local is_dir = h and h.cha.is_dir
-  if mode == "copy" then
-    if is_dir then
-      ya.manager_emit("enter", {})
-    end
-    ya.manager_emit("yank", {})
-    ya.manager_emit("paste", {})
-    ya.manager_emit("unyank", {})
-    if is_dir then
-      ya.manager_emit("leave", {})
-    end
-  elseif mode == "move" then
-    if is_dir then
-      ya.manager_emit("enter", {})
-    end
-    ya.manager_emit("yank", {cut = true})
-    ya.manager_emit("paste", {})
-    ya.manager_emit("unyank", {})
-    if is_dir then
-      ya.manager_emit("leave", {})
-    end
-  elseif mode == "copy-dir" then
-    local dir = Url("Folder with selected items")
-    if is_dir then
-      dir = h.url:join(dir)
-    else
-      dir = h.url:parent():join(dir)
-    end
-    dir = tostring(dir)
-    local cmd = string.format([[mkdir -p '%s'; cp -a "$@" '%s'; ya emit reveal '%s'; ya emit unyank]], dir, dir, dir)
-    ya.manager_emit("shell", { cmd })
-  elseif mode == "move-dir" then
-    local dir = Url("Folder with selected items")
-    if is_dir then
-      dir = h.url:join(dir)
-    else
-      dir = h.url:parent():join(dir)
-    end
-    local cmd = string.format([[mkdir -p '%s'; mv "$@" '%s'; ya emit reveal '%s'; ya emit unyank]], tostring(dir), tostring(dir), tostring(dir))
-    ya.manager_emit("shell", { cmd })
-  elseif mode == "symlink" then
-    if is_dir then
-      ya.manager_emit("enter", {})
-    end
-    ya.manager_emit("yank", {})
-    ya.manager_emit("link", {})
-    ya.manager_emit("unyank", {})
-    if is_dir then
-      ya.manager_emit("leave", {})
-    end
-  elseif mode == "hardlink" then
-    if is_dir then
-      ya.manager_emit("enter", {})
-    end
-    ya.manager_emit("yank", {})
-    ya.manager_emit("hardlink", {follow = true})
-    ya.manager_emit("unyank", {})
-    if is_dir then
-      ya.manager_emit("leave", {})
-    end
-  elseif mode == "delete" then
-    ya.manager_emit("remove", {})
-  elseif mode == "edit" then
-    if os.getenv("TMUX_POPUP") then
-      local cmd = os.getenv("XDG_CONFIG_HOME") .. "/tmux/scripts/open_in_vim.sh '' \"$@\"; tmux popup -C"
-      ya.manager_emit("shell", { cmd })
-    else
-      ya.manager_emit("open", {})
-    end
-  elseif mode == "rename" then
-    ya.manager_emit("rename", {})
-  end
-  ya.manager_emit("escape", {})
+	local selected = #cx.active.selected
+	for i = 1, #cx.tabs do
+		for _, url in pairs(cx.tabs[i].selected) do
+			ya.manager_emit("toggle", { tostring(url), state = "on" })
+			selected = selected + 1
+		end
+	end
+	if selected == 0 then
+		return
+	end
+	local h = cx.active.current.hovered
+	local is_dir = h and h.cha.is_dir
+	if mode == "copy" then
+		if is_dir then
+			ya.manager_emit("enter", {})
+		end
+		ya.manager_emit("yank", {})
+		ya.manager_emit("paste", {})
+		ya.manager_emit("unyank", {})
+		if is_dir then
+			ya.manager_emit("leave", {})
+		end
+	elseif mode == "move" then
+		if is_dir then
+			ya.manager_emit("enter", {})
+		end
+		ya.manager_emit("yank", { cut = true })
+		ya.manager_emit("paste", {})
+		ya.manager_emit("unyank", {})
+		if is_dir then
+			ya.manager_emit("leave", {})
+		end
+	elseif mode == "copy-dir" then
+		local dir = Url("Folder with selected items")
+		if is_dir then
+			dir = h.url:join(dir)
+		else
+			dir = h.url:parent():join(dir)
+		end
+		dir = tostring(dir)
+		local cmd =
+			string.format([[mkdir -p '%s'; cp -a "$@" '%s'; ya emit reveal '%s'; ya emit unyank]], dir, dir, dir)
+		ya.manager_emit("shell", { cmd })
+	elseif mode == "move-dir" then
+		local dir = Url("Folder with selected items")
+		if is_dir then
+			dir = h.url:join(dir)
+		else
+			dir = h.url:parent():join(dir)
+		end
+		local cmd = string.format(
+			[[mkdir -p '%s'; mv "$@" '%s'; ya emit reveal '%s'; ya emit unyank]],
+			tostring(dir),
+			tostring(dir),
+			tostring(dir)
+		)
+		ya.manager_emit("shell", { cmd })
+	elseif mode == "symlink" then
+		if is_dir then
+			ya.manager_emit("enter", {})
+		end
+		ya.manager_emit("yank", {})
+		ya.manager_emit("link", {})
+		ya.manager_emit("unyank", {})
+		if is_dir then
+			ya.manager_emit("leave", {})
+		end
+	elseif mode == "hardlink" then
+		if is_dir then
+			ya.manager_emit("enter", {})
+		end
+		ya.manager_emit("yank", {})
+		ya.manager_emit("hardlink", { follow = true })
+		ya.manager_emit("unyank", {})
+		if is_dir then
+			ya.manager_emit("leave", {})
+		end
+	elseif mode == "delete" then
+		ya.manager_emit("remove", {})
+	elseif mode == "edit" then
+		if os.getenv("TMUX_POPUP") then
+			local cmd = os.getenv("XDG_CONFIG_HOME") .. "/tmux/scripts/open_in_vim.sh '' \"$@\"; tmux popup -C"
+			ya.manager_emit("shell", { cmd })
+		else
+			ya.manager_emit("open", {})
+		end
+	elseif mode == "rename" then
+		ya.manager_emit("rename", {})
+	end
+	ya.manager_emit("escape", {})
 end
 
 M.smart = function(arg)
-  if arg == "enter" then
-    local h = cx.active.current.hovered
-    if not h then return end
-    if os.getenv("TMUX_POPUP") then
-      local cmd = ""
-      if h.cha.is_dir then
-        local script = "find_empty_shell"
-        cmd = string.format("%s/tmux/scripts/%s.sh '' cd ", os.getenv("XDG_CONFIG_HOME"), script)
-      else
-        local files = cx.active.current.files
-        for i = 1, #files do
-          if files[i]:is_hovered() then
-            local mime = files[i]:mime()
-            if mime:find("^text/") then
-              local script = "open_in_vim"
-              cmd = string.format("%s/tmux/scripts/%s.sh '' ", os.getenv("XDG_CONFIG_HOME"), script)
-              break
-            else
-              ya.manager_emit("open", { hovered = true })
-              return
-            end
-          end
-        end
-      end
-      cmd = cmd .. ya.quote(tostring(h.url)) .. "; tmux popup -C"
-      ya.manager_emit("shell", { cmd })
-    else
-      if h.cha.is_dir then
-        ya.manager_emit("shell", { "cd '"..tostring(h.url).."'; $SHELL -l", block = true })
-      else
-        ya.manager_emit("open", { hovered = true })
-      end
-    end
-  elseif arg == "alt-enter" then
-    if not os.getenv("TMUX") then return end
-    local h = cx.active.current.hovered
-    if not h then return end
-    local script = h.cha.is_dir and "find_empty_shell" or "open_in_vim"
-    local cmd = string.format("%s/tmux/scripts/%s.sh '' -n ", os.getenv("XDG_CONFIG_HOME"), script)
-    if h.cha.is_dir then
-      cmd = cmd .. "cd "
-    end
-    cmd = cmd .. ya.quote(tostring(h.url)) .. "; tmux popup -C"
-    ya.manager_emit("shell", { cmd })
-  elseif arg == "esc" then
-    if #cx.yanked > 0 then
-      ya.manager_emit("unyank", {})
-    else
-      ya.manager_emit("escape", {})
-    end
-  elseif arg == "up" then
-    local cursor = cx.active.current.cursor
-    if cursor == 0 then
-      ya.manager_emit("arrow", { 99999999 })
-    else
-      ya.manager_emit("arrow", { -1 })
-    end
-  elseif arg == "down" then
-    local cursor = cx.active.current.cursor
-    local length = #cx.active.current.files
-    if cursor == length - 1 then
-      ya.manager_emit("arrow", { -99999999 })
-    else
-      ya.manager_emit("arrow", { 1 })
-    end
-  elseif arg == "parent-up" then
-    local parent = cx.active.parent
-    if not parent then return end
-    local target = parent.files[parent.cursor]
-    if target and target.cha.is_dir then
-      ya.manager_emit("cd", { target.url })
-    end
-  elseif arg == "parent-down" then
-    local parent = cx.active.parent
-    if not parent then return end
-    local target = parent.files[parent.cursor + 2]
-    if target and target.cha.is_dir then
-      ya.manager_emit("cd", { target.url })
-    end
-  elseif arg == "new" then
-    local files = cx.active.current.files
-    for i = 1, #files do
-      if files[i]:found() then
-        ya.manager_emit("find_arrow", { previous = true })
-        return
-      end
-    end
-    ya.manager_emit("create", {})
-  elseif arg == "tab" then
-    local h = cx.active.current.hovered
-    ya.manager_emit("tab_create", h and h.cha.is_dir and { h.url } or { current = true })
-  elseif arg == "next-tab" then
-    if #cx.tabs == 1 then
-      local h = cx.active.current.hovered
-      ya.manager_emit("tab_create", h and h.cha.is_dir and { h.url } or { current = true })
-    else
-      ya.manager_emit("tab_switch", { 1, relative = true })
-    end
-  end
+	if arg == "enter" then
+		local h = cx.active.current.hovered
+		if not h then
+			return
+		end
+		if os.getenv("TMUX_POPUP") then
+			local cmd = ""
+			if h.cha.is_dir then
+				local script = "find_empty_shell"
+				cmd = string.format("%s/tmux/scripts/%s.sh '' cd ", os.getenv("XDG_CONFIG_HOME"), script)
+			else
+				local files = cx.active.current.files
+				for i = 1, #files do
+					if files[i]:is_hovered() then
+						local mime = files[i]:mime()
+						if mime:find("^text/") then
+							local script = "open_in_vim"
+							cmd = string.format("%s/tmux/scripts/%s.sh '' ", os.getenv("XDG_CONFIG_HOME"), script)
+							break
+						else
+							ya.manager_emit("open", { hovered = true })
+							return
+						end
+					end
+				end
+			end
+			cmd = cmd .. ya.quote(tostring(h.url)) .. "; tmux popup -C"
+			ya.manager_emit("shell", { cmd })
+		else
+			if h.cha.is_dir then
+				ya.manager_emit("shell", { "cd '" .. tostring(h.url) .. "'; $SHELL -l", block = true })
+			else
+				ya.manager_emit("open", { hovered = true })
+			end
+		end
+	elseif arg == "alt-enter" then
+		if not os.getenv("TMUX") then
+			return
+		end
+		local h = cx.active.current.hovered
+		if not h then
+			return
+		end
+		local script = h.cha.is_dir and "find_empty_shell" or "open_in_vim"
+		local cmd = string.format("%s/tmux/scripts/%s.sh '' -n ", os.getenv("XDG_CONFIG_HOME"), script)
+		if h.cha.is_dir then
+			cmd = cmd .. "cd "
+		end
+		cmd = cmd .. ya.quote(tostring(h.url)) .. "; tmux popup -C"
+		ya.manager_emit("shell", { cmd })
+	elseif arg == "esc" then
+		if #cx.yanked > 0 then
+			ya.manager_emit("unyank", {})
+		else
+			ya.manager_emit("escape", {})
+		end
+	elseif arg == "up" then
+		local cursor = cx.active.current.cursor
+		if cursor == 0 then
+			ya.manager_emit("arrow", { 99999999 })
+		else
+			ya.manager_emit("arrow", { -1 })
+		end
+	elseif arg == "down" then
+		local cursor = cx.active.current.cursor
+		local length = #cx.active.current.files
+		if cursor == length - 1 then
+			ya.manager_emit("arrow", { -99999999 })
+		else
+			ya.manager_emit("arrow", { 1 })
+		end
+	elseif arg == "parent-up" then
+		local parent = cx.active.parent
+		if not parent then
+			return
+		end
+		local target = parent.files[parent.cursor]
+		if target and target.cha.is_dir then
+			ya.manager_emit("cd", { target.url })
+		end
+	elseif arg == "parent-down" then
+		local parent = cx.active.parent
+		if not parent then
+			return
+		end
+		local target = parent.files[parent.cursor + 2]
+		if target and target.cha.is_dir then
+			ya.manager_emit("cd", { target.url })
+		end
+	elseif arg == "new" then
+		local files = cx.active.current.files
+		for i = 1, #files do
+			if files[i]:found() then
+				ya.manager_emit("find_arrow", { previous = true })
+				return
+			end
+		end
+		ya.manager_emit("create", {})
+	elseif arg == "tab" then
+		local h = cx.active.current.hovered
+		ya.manager_emit("tab_create", h and h.cha.is_dir and { h.url } or { current = true })
+	elseif arg == "next-tab" then
+		if #cx.tabs == 1 then
+			local h = cx.active.current.hovered
+			ya.manager_emit("tab_create", h and h.cha.is_dir and { h.url } or { current = true })
+		else
+			ya.manager_emit("tab_switch", { 1, relative = true })
+		end
+	end
 end
 
 return {
-  entry = function(_, job)
-    local args = job.args
-    local func = M[args[1]]
-    if func ~= nil then
-      table.remove(args, 1)
-      return func(table.unpack(args))
-    end
-  end
+	entry = function(_, job)
+		local args = job.args
+		local func = M[args[1]]
+		if func ~= nil then
+			table.remove(args, 1)
+			return func(table.unpack(args))
+		end
+	end,
 }
--- my-fzf.yazi/init.lua
+++ my-fzf.yazi/init.lua
@@ -1,254 +1,273 @@
+--- @diagnostic disable: undefined-global
 _G.ya = _G.ya or {}
 _G.cx = _G.cx or {}
 _G.Command = _G.Command or {}
 
--- local function fail(s, ...) ya.notify { title = "fzf", content = s:format(...), timeout = 5, level = "error" } end
-
 local M = {}
 
 M.z = function(s)
-  local cwd = s.cwd
-  ya.hide()
-  local _z = ":reload:zoxide query {q} -l --exclude $PWD || true"
-  local child = Command("fzf")
-    :args({"--bind", "start".._z})
-    :args({"--bind", "change".._z})
-    :args({"--bind", "ctrl-t:print(tab)+accept"})
-    :args({"--header", "\x1b[1;36m⌃T\x1b[0m Open in a new tab"})
-    :args({"--disabled", "--preview-window=up,60%"})
-    :args({"--preview", "fzf-preview {}"})
-    :cwd(cwd)
-    :stdout(Command.PIPED)
-    :spawn()
-  local lines = {}
-  while true do
-    local line, event = child:read_line()
-    if event ~= 0 then break end
-    line = line:gsub("\n", "")
-    table.insert(lines, line)
-  end
-  if #lines == 0 then return end
-  if #lines == 1 then
-    ya.manager_emit("cd", { lines[1] })
-  elseif lines[1] == "tab" then
-    ya.manager_emit("tab_create", { lines[2] })
-  end
+	local cwd = s.cwd
+	ya.hide()
+	local _z = ":reload:zoxide query {q} -l --exclude $PWD || true"
+	local child = Command("fzf")
+		:args({ "--bind", "start" .. _z })
+		:args({ "--bind", "change" .. _z })
+		:args({ "--bind", "ctrl-t:print(tab)+accept" })
+		:args({ "--header", "\x1b[1;36m⌃T\x1b[0m Open in a new tab" })
+		:args({ "--disabled", "--preview-window=up,60%" })
+		:args({ "--preview", "fzf-preview {}" })
+		:cwd(cwd)
+		:stdout(Command.PIPED)
+		:spawn()
+	local lines = {}
+	while true do
+		local line, event = child:read_line()
+		if event ~= 0 then
+			break
+		end
+		line = line:gsub("\n", "")
+		table.insert(lines, line)
+	end
+	if #lines == 0 then
+		return
+	end
+	if #lines == 1 then
+		ya.manager_emit("cd", { lines[1] })
+	elseif lines[1] == "tab" then
+		ya.manager_emit("tab_create", { lines[2] })
+	end
 end
 
 M.fd = function(s)
-  local cwd = s.cwd
-  ya.hide()
-  local _hd = function(t)
-    local types = {
-      f = "file",
-      d = "dir",
-      l = "symlink",
-      s = "socket",
-      x = "executable",
-    }
-    local header = {}
-    local BOLD="\x1b[1;36m"
-    local OFF="\x1b[0m"
-    for k, v in pairs(types) do
-      local h = t == k and BOLD or ""
-      h = h .. string.format("⌥%s %s", string.upper(k), v)
-      h = h .. (t == k and OFF or "")
-      table.insert(header, h)
-    end
-    return table.concat(header, " / ")
-  end
-  local _fd = function(k, t)
-    return string.format("%s:reload(fd -t%s -HL --strip-cwd-prefix=always .)+change-header( %s )", k, t, _hd(t))
-  end
-  local child = Command("fzf")
-    :args({"--preview", "fzf-preview {}", "--preview-window=up,60%", "-m"})
-    :args({"--bind", _fd("start", "f")})
-    :args({"--bind", _fd("alt-d", "d")})
-    :args({"--bind", _fd("alt-l", "l")})
-    :args({"--bind", _fd("alt-s", "s")})
-    :args({"--bind", _fd("alt-f", "f")})
-    :args({"--bind", _fd("alt-x", "x")})
-    :cwd(cwd)
-    :stdout(Command.PIPED)
-    :spawn()
-  local files = {}
-  while true do
-    local file, event = child:read_line()
-    if event ~= 0 then break end
-    file = file:gsub("\n", "")
-    table.insert(files, file)
-  end
-  if #files == 1 then
-    local cha, err = fs.cha(Url(cwd):join(Url(files[1])), true)
-    if err then
-      return
-    end
-    ya.manager_emit(cha.is_dir and "cd" or "reveal", { files[1] })
-  elseif #files > 1 then
-    local last_file
-    for _, file in ipairs(files) do
-      file = tostring(Url(cwd):join(Url(file)))
-      ya.manager_emit("toggle", { file, state = "on" })
-      last_file = file
-    end
-    ya.manager_emit("reveal", { last_file })
-  end
+	local cwd = s.cwd
+	ya.hide()
+	local _hd = function(t)
+		local types = {
+			f = "file",
+			d = "dir",
+			l = "symlink",
+			s = "socket",
+			x = "executable",
+		}
+		local header = {}
+		local BOLD = "\x1b[1;36m"
+		local OFF = "\x1b[0m"
+		for k, v in pairs(types) do
+			local h = t == k and BOLD or ""
+			h = h .. string.format("⌥%s %s", string.upper(k), v)
+			h = h .. (t == k and OFF or "")
+			table.insert(header, h)
+		end
+		return table.concat(header, " / ")
+	end
+	local _fd = function(k, t)
+		return string.format("%s:reload(fd -t%s -HL --strip-cwd-prefix=always .)+change-header( %s )", k, t, _hd(t))
+	end
+	local child = Command("fzf")
+		:args({ "--preview", "fzf-preview {}", "--preview-window=up,60%", "-m" })
+		:args({ "--bind", _fd("start", "f") })
+		:args({ "--bind", _fd("alt-d", "d") })
+		:args({ "--bind", _fd("alt-l", "l") })
+		:args({ "--bind", _fd("alt-s", "s") })
+		:args({ "--bind", _fd("alt-f", "f") })
+		:args({ "--bind", _fd("alt-x", "x") })
+		:cwd(cwd)
+		:stdout(Command.PIPED)
+		:spawn()
+	local files = {}
+	while true do
+		local file, event = child:read_line()
+		if event ~= 0 then
+			break
+		end
+		file = file:gsub("\n", "")
+		table.insert(files, file)
+	end
+	if #files == 1 then
+		local cha, err = fs.cha(Url(cwd):join(Url(files[1])), true)
+		if err then
+			return
+		end
+		ya.manager_emit(cha.is_dir and "cd" or "reveal", { files[1] })
+	elseif #files > 1 then
+		local last_file
+		for _, file in ipairs(files) do
+			file = tostring(Url(cwd):join(Url(file)))
+			ya.manager_emit("toggle", { file, state = "on" })
+			last_file = file
+		end
+		ya.manager_emit("reveal", { last_file })
+	end
 end
 
 M.fif = function(s)
-  local cwd = s.cwd
-  ya.hide()
-  local child = Command("fif")
-    :args({"-o"})
-    :cwd(cwd)
-    :stdout(Command.PIPED)
-    :spawn()
-  local files = {}
-  local ln
-  while true do
-    local line, event = child:read_line()
-    if event ~= 0 then break end
-    local file = line:match("^[^:\n]+")
-    ln = line:match("^[^:]+:(%d+)")
-    table.insert(files, file)
-  end
-  if #files == 0 then return end
-  if os.getenv("TMUX_POPUP") then
-    if #files == 1 then
-      ya.manager_emit("reveal", { files[1] })
-    else
-      local last_file
-      for _, file in ipairs(files) do
-        file = tostring(Url(cwd):join(Url(file)))
-        ya.manager_emit("toggle", { file, state = "on" })
-        last_file = file
-      end
-      ya.manager_emit("reveal", { last_file })
-    end
-  else
-    local args = ""
-    if #files == 1 then
-      args = args .. ya.quote(files[1])
-      if ln then
-        args = args .. " +" .. ln
-      end
-    else
-      for _, file in ipairs(files) do
-        args = args .. ya.quote(file) .. " "
-      end
-    end
-    ya.manager_emit("shell", { "nvim "..args, block = true })
-  end
+	local cwd = s.cwd
+	ya.hide()
+	local child = Command("fif"):args({ "-o" }):cwd(cwd):stdout(Command.PIPED):spawn()
+	local files = {}
+	local ln
+	while true do
+		local line, event = child:read_line()
+		if event ~= 0 then
+			break
+		end
+		local file = line:match("^[^:\n]+")
+		ln = line:match("^[^:]+:(%d+)")
+		table.insert(files, file)
+	end
+	if #files == 0 then
+		return
+	end
+	if os.getenv("TMUX_POPUP") then
+		if #files == 1 then
+			ya.manager_emit("reveal", { files[1] })
+		else
+			local last_file
+			for _, file in ipairs(files) do
+				file = tostring(Url(cwd):join(Url(file)))
+				ya.manager_emit("toggle", { file, state = "on" })
+				last_file = file
+			end
+			ya.manager_emit("reveal", { last_file })
+		end
+	else
+		local args = ""
+		if #files == 1 then
+			args = args .. ya.quote(files[1])
+			if ln then
+				args = args .. " +" .. ln
+			end
+		else
+			for _, file in ipairs(files) do
+				args = args .. ya.quote(file) .. " "
+			end
+		end
+		ya.manager_emit("shell", { "nvim " .. args, block = true })
+	end
 end
 
 M.git = function(s)
-  local cwd = s.cwd
-  ya.hide()
-  local child = Command("awk")
-    :arg("/recentrepos:/ {found=1; next} found && /^[^[:space:]]/ {exit} found {print}")
-    :arg(os.getenv("XDG_STATE_HOME") .. "/lazygit/state.yml")
-    :stdout(Command.PIPED)
-    :spawn()
-  local repos = {}
-  while true do
-    local repo, event = child:read_line()
-    if event ~= 0 then break end
-    repo = repo:gsub("^ +- ", ""):gsub("\n", "")
-    if repo ~= "" and repo ~= cwd then
-      table.insert(repos, repo)
-    end
-  end
-  child = Command("fzf")
-    :args({"--preview", [[echo -e "\033[1m$(basename {})\033[0m\n"; git -c color.status=always -C {} status -bs]], "--preview-window=wrap,up,60%"})
-    :stdin(Command.PIPED)
-    :stdout(Command.PIPED)
-    :spawn()
-  child:write_all(table.concat(repos, "\n"))
-  child:flush()
-  local selected = child:wait_with_output().stdout:gsub("\n", "")
-  if selected ~= "" then
-    ya.manager_emit("cd", { selected })
-  end
+	local cwd = s.cwd
+	ya.hide()
+	local child = Command("awk")
+		:arg("/recentrepos:/ {found=1; next} found && /^[^[:space:]]/ {exit} found {print}")
+		:arg(os.getenv("XDG_STATE_HOME") .. "/lazygit/state.yml")
+		:stdout(Command.PIPED)
+		:spawn()
+	local repos = {}
+	while true do
+		local repo, event = child:read_line()
+		if event ~= 0 then
+			break
+		end
+		repo = repo:gsub("^ +- ", ""):gsub("\n", "")
+		if repo ~= "" and repo ~= cwd then
+			table.insert(repos, repo)
+		end
+	end
+	child = Command("fzf")
+		:args({
+			"--preview",
+			[[echo -e "\033[1m$(basename {})\033[0m\n"; git -c color.status=always -C {} status -bs]],
+			"--preview-window=wrap,up,60%",
+		})
+		:stdin(Command.PIPED)
+		:stdout(Command.PIPED)
+		:spawn()
+	child:write_all(table.concat(repos, "\n"))
+	child:flush()
+	local selected = child:wait_with_output().stdout:gsub("\n", "")
+	if selected ~= "" then
+		ya.manager_emit("cd", { selected })
+	end
 end
 
 M.obsearch = function()
-  ya.hide()
-  local child = Command("obsearch")
-    :args({"-o"})
-    :stdout(Command.PIPED)
-    :spawn()
-  local files = {}
-  local ln
-  while true do
-    local line, event = child:read_line()
-    if event ~= 0 then break end
-    local file = line:match("^[^:\n]+")
-    ln = line:match("^[^:]+:(%d+)")
-    table.insert(files, file)
-  end
-  if #files == 0 then return end
-  if os.getenv("TMUX_POPUP") then
-    if #files == 1 then
-      ya.manager_emit("reveal", { files[1] })
-    else
-      local last_file
-      for _, file in ipairs(files) do
-        ya.manager_emit("toggle", { file, state = "on" })
-        last_file = file
-      end
-      ya.manager_emit("reveal", { last_file })
-    end
-  else
-    local args = ""
-    if #files == 1 then
-      args = args .. ya.quote(files[1])
-      if ln then
-        args = args .. " +" .. ln
-      end
-    else
-      for _, file in ipairs(files) do
-        args = args .. ya.quote(file) .. " "
-      end
-    end
-    ya.manager_emit("shell", { "nvim "..args, block = true })
-  end
+	ya.hide()
+	local child = Command("obsearch"):args({ "-o" }):stdout(Command.PIPED):spawn()
+	local files = {}
+	local ln
+	while true do
+		local line, event = child:read_line()
+		if event ~= 0 then
+			break
+		end
+		local file = line:match("^[^:\n]+")
+		ln = line:match("^[^:]+:(%d+)")
+		table.insert(files, file)
+	end
+	if #files == 0 then
+		return
+	end
+	if os.getenv("TMUX_POPUP") then
+		if #files == 1 then
+			ya.manager_emit("reveal", { files[1] })
+		else
+			local last_file
+			for _, file in ipairs(files) do
+				ya.manager_emit("toggle", { file, state = "on" })
+				last_file = file
+			end
+			ya.manager_emit("reveal", { last_file })
+		end
+	else
+		local args = ""
+		if #files == 1 then
+			args = args .. ya.quote(files[1])
+			if ln then
+				args = args .. " +" .. ln
+			end
+		else
+			for _, file in ipairs(files) do
+				args = args .. ya.quote(file) .. " "
+			end
+		end
+		ya.manager_emit("shell", { "nvim " .. args, block = true })
+	end
 end
 
 M.selected = function(s)
-  local selected = s.selected
-  if #selected == 0 then return end
-  ya.hide()
-  local tmpfile = Command("mktemp"):args({"/tmp/yazi.XXXXXX"}):stdout(Command.PIPED):output().stdout:gsub("\n", "")
-  ya.manager_emit("shell", { [[printf '%s\n' "$@" > ]]..tmpfile })
-  local output = Command("fzf")
-    :args({"--preview", "fzf-preview {}", "--preview-window", "up,60%"})
-    :args({"--bind", "start:reload:cat "..tmpfile})
-    :args({"--bind", [[ctrl-x:reload:ya emit toggle {} --state=off && ya emit shell 'printf "%s\n" "$@" > ]]..tmpfile.."' && sleep 0.1 && cat "..tmpfile})
-    :args({"--header", "\x1b[1;36m⌃X\x1b[0m Deselect"})
-    :stdout(Command.PIPED)
-    :output()
-  local file = output.stdout:gsub("\n", "")
-  if file ~= "" then
-    ya.manager_emit("reveal", { file })
-  end
-  ya.manager_emit("shell", { "rm "..tmpfile })
+	local selected = s.selected
+	if #selected == 0 then
+		return
+	end
+	ya.hide()
+	local tmpfile = Command("mktemp"):args({ "/tmp/yazi.XXXXXX" }):stdout(Command.PIPED):output().stdout:gsub("\n", "")
+	ya.manager_emit("shell", { [[printf '%s\n' "$@" > ]] .. tmpfile })
+	local output = Command("fzf")
+		:args({ "--preview", "fzf-preview {}", "--preview-window", "up,60%" })
+		:args({ "--bind", "start:reload:cat " .. tmpfile })
+		:args({
+			"--bind",
+			[[ctrl-x:reload:ya emit toggle {} --state=off && ya emit shell 'printf "%s\n" "$@" > ]]
+				.. tmpfile
+				.. "' && sleep 0.1 && cat "
+				.. tmpfile,
+		})
+		:args({ "--header", "\x1b[1;36m⌃X\x1b[0m Deselect" })
+		:stdout(Command.PIPED)
+		:output()
+	local file = output.stdout:gsub("\n", "")
+	if file ~= "" then
+		ya.manager_emit("reveal", { file })
+	end
+	ya.manager_emit("shell", { "rm " .. tmpfile })
 end
 
 local state = ya.sync(function()
-  local selected = {}
-  for _, url in pairs(cx.active.selected) do
-    table.insert(selected, tostring(url))
-  end
-  return {
-    cwd = tostring(cx.active.current.cwd),
-    selected = selected,
-  }
+	local selected = {}
+	for _, url in pairs(cx.active.selected) do
+		table.insert(selected, tostring(url))
+	end
+	return {
+		cwd = tostring(cx.active.current.cwd),
+		selected = selected,
+	}
 end)
 
 return {
-  entry = function(_, job)
-    local s = state()
-    M[job.args[1]](s)
-  end
+	entry = function(_, job)
+		local s = state()
+		M[job.args[1]](s)
+	end,
 }
-
